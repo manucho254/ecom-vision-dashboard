2f765ca5a8dc00efdc8ce43171399dc975e42361
Emmanuel manucho <rampagetest254@gmail.com>
2022-09-27 19:22:11 +0300

    initial

--- src/main.js
+++ src/main.js
@@ -1,8 +1,27 @@
+//core apps
 import Vue from 'vue'
 import App from './App.vue'
+
+//third party apps
+import router from './router'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import Vuelidate from 'vuelidate';
+
+// third party css
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+// custom css
+import '@/assets/css/styles.css'
+
+
+Vue.use(BootstrapVue)
+Vue.use(IconsPlugin)
+Vue.use(Vuelidate)
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  router,
+  render: h => h(App)
 }).$mount('#app')
